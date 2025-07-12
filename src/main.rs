f40e1ad546c2caef02c0bf5b83c9909fbb3cc4d6
Decator <decator.c@proton.me>
2025-07-12 00:05:51 -0400

    VIEWPORT. WTF

--- src/main.rs
+++ src/main.rs
@@ -308,6 +308,7 @@ fn generate_html(releases: &[Release]) -> String {
         r#"
          <head>
          <meta charset="utf-8" />
+         <meta name="viewport" content="width=device-width, initial-scale=1">
          <title>{}</title>
          <meta property="og:title" content="{}" />
          <style>
